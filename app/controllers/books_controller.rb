267d7356564629c055068db45066f96a42c6990f
marsh-sb <enotaku5526@gmail.com>
2020-11-23 06:16:43 +0000

    [add] 投稿機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,17 +1,23 @@
 class BooksController < ApplicationController
+  
   def index
+    @books = Book.all
+    @book = Book.new
   end
 
   def show
-    
   end
   
   def create
-  
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      render 'index'
+    end
   end
 
   def edit
-  
   end
 
   def update
@@ -25,5 +31,4 @@ class BooksController < ApplicationController
   def book_params
     params.require(:book).permit(:title, :body)
   end
-
 end
